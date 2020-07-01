ffe1e7288503c01084b6544e0bd2d343f25b39c8
David Ghiassi <david.ghiassi@epita.fr>
2020-07-01 16:06:37 +0200

    Included some libraries...

--- src/main.cc
+++ src/main.cc
@@ -1,4 +1,18 @@
-int main()
+#include <Python.h>
+#include <boost/python.hpp>
+#include <boost/python/numpy.hpp>
+#include <mln/core/image/ndbuffer_image.hpp>
+
+mln::ndbuffer_image numpy_to_buffer_image(PyObject *obj)
 {
-    return 0;
+    /*
+    PyArrayObject *arr = getNPArray();
+    *m = getShape(arr, 0);
+    *n = getShape(arr, 1);
+    */
+}
+
+PyObject* buffer_image_to_numpy(mln::ndbuffer_image buffer_image)
+{
+
 }
